5a677151d3a90c1efda850e43cd5625f9d9d51bf
vetonmilaimi <vetonmila@gmail.com>
2024-10-04 01:53:36 +0200

    Add employees table

--- src/Pages/Admin/Users.tsx
+++ src/Pages/Admin/Users.tsx
@@ -1,69 +1,20 @@
-import { App, Button, Popconfirm, Space, Spin, Table, TableProps } from "antd";
+import { App, Button } from "antd";
 import { ErrorResponse, IUser } from "../../utils/types";
 import { useEffect, useState } from "react";
-import { APP_URL, USER_ROLES } from "../../utils/constants";
 import { GlobalSliceReducers } from "../../store/slices/global.slice";
-import { store } from "../../store/store";
+import { RootState, store } from "../../store/store";
 import AddUser from "../../Components/Forms/AddUser.form";
-import superAdminService from "../../services/admin.service";
+import adminService from "../../services/admin.service";
 import AppTexts from "../../utils/texts/app-texts.json";
+import CustomUsersTable from "../../Components/UI/CustomUsersTable";
+import { useSelector } from "react-redux";
 
 const Users = () => {
+  const auth = useSelector((state: RootState) => state.auth.value);
   const [users, setUsers] = useState<IUser[]>([]);
   const [loading, setLoading] = useState(true);
 
   const { notification } = App.useApp();
-
-  const columns: TableProps<IUser>["columns"] = [
-    {
-      title: "Name",
-      key: "firstName",
-      render: (_, record) => `${record.firstName} ${record.lastName}`,
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-    {
-      title: "Role",
-      dataIndex: "role",
-      key: "role",
-    },
-    {
-      title: "Activate Url",
-      dataIndex: "verifyToken",
-      key: "verifyToken",
-      render: (_, record) =>
-        record?.activateToken
-          ? `${APP_URL}/activate?token=${record.activateToken}`
-          : "Activated",
-    },
-    {
-      title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Space size="middle">
-          <Popconfirm
-            title={AppTexts.users_page["delete-user"]}
-            onConfirm={() => deleteUser(record._id)}
-            onCancel={() => {}}
-            okText={AppTexts.global.yes}
-            cancelText={AppTexts.global.no}
-          >
-            <Button
-              disabled={record.role === USER_ROLES.ADMIN}
-              className="bg-red-500 text-white"
-              type="default"
-              onClick={() => {}}
-            >
-              {AppTexts.global.delete}
-            </Button>
-          </Popconfirm>
-        </Space>
-      ),
-    },
-  ];
 
   const addUser = () => {
     store.dispatch(
@@ -82,32 +33,13 @@ const Users = () => {
 
   const getUsers = async () => {
     try {
-      const response = await superAdminService.listUsers();
+      const response = await adminService.listUsers();
       setUsers(
         response.message.map((el) => {
           return { ...el, key: el._id };
         })
       );
     } catch (error: unknown) {
-      notification.error({
-        message: (error as ErrorResponse).name,
-        description: (error as ErrorResponse).message,
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const deleteUser = async (_id: string) => {
-    setLoading(true);
-    try {
-      const response = await superAdminService.deleteUser({
-        query: {
-          _id,
-        },
-      });
-      if (!response.error) getUsers();
-    } catch (error) {
       notification.error({
         message: (error as ErrorResponse).name,
         description: (error as ErrorResponse).message,
@@ -129,16 +61,12 @@ const Users = () => {
           {AppTexts.users_page["add-user"]}
         </Button>
       </div>
-      <Table
-        loading={
-          loading && {
-            indicator: <Spin size="large" />,
-          }
-        }
-        tableLayout="fixed"
-        columns={columns}
+      <CustomUsersTable
+        loading={loading}
+        getUsers={getUsers}
+        setLoading={setLoading}
         dataSource={users}
-        pagination={false}
+        currentRole={auth.user?.role}
       />
     </div>
   );

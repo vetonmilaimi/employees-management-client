3ca99aec61a64e12620fae0636a7ff08d085bac7
vetonmilaimi <vetonmila@gmail.com>
2025-03-01 23:36:01 +0100

    Some formatting

--- src/Components/Forms/AddJobEvent.form.tsx
+++ src/Components/Forms/AddJobEvent.form.tsx
@@ -1,4 +1,4 @@
-import { Form, Input } from "antd";
+import { Button, Form, Input, Select } from "antd";
 import { useForm } from "antd/es/form/Form";
 import TextArea from "antd/es/input/TextArea";
 
@@ -11,16 +11,47 @@ const AddJobEvent = () => {
   return (
     <div className="px-5">
       <h1 className="text-xl mb-1 text-secondary">Add a job event</h1>
-      <Form name="add-job-event-form" onFinish={onSubmit} form={form}>
-        <Form.Item
-          name="name"
-          rules={[{ required: true, message: "Please input job event name!" }]}
-        >
-          <Input placeholder="Job event name" />
-        </Form.Item>
-        <Form.Item name="description" rules={[{ required: false }]}>
-          <TextArea placeholder="Job event Description" />
-        </Form.Item>
+      <Form
+        name="add-job-event-form"
+        onFinish={onSubmit}
+        form={form}
+        layout="horizontal"
+      >
+        <div className="flex justify-center gap-4">
+          <Form.Item
+            name="name"
+            className="w-[100%]"
+            vertical
+            rules={[
+              { required: true, message: "Please input job event title!" },
+            ]}
+          >
+            <Input placeholder="Job event title" />
+          </Form.Item>
+        </div>
+        <div className="flex justify-center gap-4">
+          <Form.Item
+            className="w-2/3"
+            name="description"
+            rules={[{ required: false }]}
+          >
+            <TextArea placeholder="Job event Description" rows={4} />
+          </Form.Item>
+          <Form.Item className="w-1/3">
+            <Select placeholder="Select a project">
+              <Select.Option value="1">Project 1</Select.Option>
+              <Select.Option value="2">Project 2</Select.Option>
+            </Select>
+          </Form.Item>
+        </div>
+
+        <div className="flex justify-end">
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              Submit
+            </Button>
+          </Form.Item>
+        </div>
       </Form>
     </div>
   );

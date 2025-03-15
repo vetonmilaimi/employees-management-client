71ce022862f424c976f6af99c2cfb583e91cf502
milv <veton.milaimi@vamed.com>
2025-03-15 19:24:40 +0100

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 3000,
+    port: 8080,
   },
   server: {
-    port: 3000,
+    port: 8080,
   },
 });
